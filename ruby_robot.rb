18fc974b200a857601e9d82974e6b3fb4764ad23
plagiatmakc <plagiatmakc@gmail.com>
2018-09-27 11:07:30 +0300

    refactoring

--- ruby_robot.rb
+++ ruby_robot.rb
@@ -5,7 +5,7 @@ class Robot
   def initialize(table_x = 5, table_y = 6)
     @table_x = table_x
     @table_y = table_y
-    @directions = ['WEST', 'NORTH', 'EAST', 'SOUTH']
+    @directions = %W['WEST', 'NORTH', 'EAST', 'SOUTH']
     p('Place your robot')
     request = gets.chomp
     while request != 'EXIT'
@@ -36,7 +36,7 @@ class Robot
   def place(x_dim, y_dim, direction)
     return p('dont place outside a table') if x_dim < 0 || x_dim > @table_x || y_dim < 0 || y_dim > @table_y
 
-    @current_pos = {:x_dim => x_dim, :y_dim => y_dim, :direction => direction}
+    @current_pos = { :x_dim => x_dim, :y_dim => y_dim, :direction => direction }
     p('Robot was placed')
   end
 
@@ -51,20 +51,20 @@ class Robot
   def move
     case @current_pos[:direction]
     when 'WEST'
-      @current_pos[:x_dim] -= 1 unless @current_pos[:x_dim] == 0
+      @current_pos[:x_dim] -= 1 unless @current_pos[:x_dim].zero?
     when 'NORTH'
       @current_pos[:y_dim] += 1 unless @current_pos[:y_dim] == @table_y
     when 'EAST'
       @current_pos[:x_dim] += 1 unless @current_pos[:x_dim] == @table_x
     when 'SOUTH'
-      @current_pos[:y_dim] -= 1 unless @current_pos[:y_dim] == 0
+      @current_pos[:y_dim] -= 1 unless @current_pos[:y_dim].zero?
     end
   end
 
   def report
     return  puts('Robot not placed') if @current_pos.nil?
+
     puts("Output: #{@current_pos[:x_dim]},#{@current_pos[:y_dim]},#{@current_pos[:direction]}")
   end
-
 end
-_run = Robot.new(10,10)
+_run = Robot.new(10, 10)
